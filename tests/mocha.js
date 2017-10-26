e10483a0ba5f55745df637d6d58b32b48282819e
Justin Ridgewell <justin@ridgewell.name>
2017-10-25 23:25:21 -0400

    Report total tests

--- tests/mocha.js
+++ tests/mocha.js
@@ -3,7 +3,13 @@
 let i = 0;
 const context = [];
 exports.describe = (description, fn) => {
+  context.push(description);
   fn();
+  context.pop(description);
+  if (context.length === 0) {
+    process.stdout.write('\n');
+    console.log(`${i} tests run`);
+  }
 };
 
 exports.it = (description, fn) => {
