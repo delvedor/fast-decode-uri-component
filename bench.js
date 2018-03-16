0689a6fa2c35c682c7a5ab84bb43095711eff3ed
delvedor <tommydelved@gmail.com>
2018-03-16 11:17:42 +0100

    Updated test and benchmarks

--- bench.js
+++ bench.js
@@ -7,7 +7,7 @@ const uri = [
   'test', 'a+b+c+d', '=a', '%25', '%%25%%', 'st%C3%A5le', 'st%C3%A5le%', '%st%C3%A5le%', '%%7Bst%C3%A5le%7D%',
   '%ab%C3%A5le%', '%C3%A5%able%', '%7B%ab%7C%de%7D', '%7B%ab%%7C%de%%7D', '%7 B%ab%%7C%de%%7 D', '%61+%4d%4D',
   '\uFEFFtest', '\uFEFF', '%EF%BB%BFtest', '%EF%BB%BF', '†', '%C2%B5', '%C2%B5%', '%%C2%B5%', '%ab', '%ab%ab%ab',
-  '%', '%E0%A4%A', '/test/hel%"Flo'
+  '%', '%E0%A4%A', '/test/hel%"Flo', '/test/hel%2Flo'
 ]
 
 const uriLen = uri.length
--- test.js
+++ test.js
@@ -4,47 +4,18 @@ const { test } = require('tap')
 const fastDecode = require('./index')
 
 test('Basic', t => {
-  const testCases = {
-    'test': 'test',
-    'a+b+c+d': 'a+b+c+d',
-    '=a': '=a',
-    '%25': '%',
-    '%%25%%': '%%%%',
-    'st%C3%A5le': 'ståle',
-    'st%C3%A5le%': 'ståle%',
-    '%st%C3%A5le%': '%ståle%',
-    '%%7Bst%C3%A5le%7D%': '%{ståle}%',
-    '%ab%C3%A5le%': '%abåle%',
-    '%C3%A5%able%': 'å%able%',
-    '%7B%ab%7C%de%7D': '{%ab|%de}',
-    '%7B%ab%%7C%de%%7D': '{%ab%|%de%}',
-    '%7 B%ab%%7C%de%%7 D': '%7 B%ab%|%de%%7 D',
-    '%61+%4d%4D': 'a+MM',
-    '\uFEFFtest': '\uFEFFtest',
-    '\uFEFF': '\uFEFF',
-    '%EF%BB%BFtest': '\uFEFFtest',
-    '%EF%BB%BF': '\uFEFF',
-    '†': '†',
-    '%C2%B5': 'µ',
-    '%C2%B5%': 'µ%',
-    '%%C2%B5%': '%µ%',
-    '%ab': null,
-    '%ab%ab%ab': null,
-    '%': null,
-    '%E0%A4%A': null,
-    '/test/hel%"Flo': null
-  }
+  const uri = [
+    'test', 'a+b+c+d', '=a', '%25', '%%25%%', 'st%C3%A5le', 'st%C3%A5le%', '%st%C3%A5le%', '%%7Bst%C3%A5le%7D%',
+    '%ab%C3%A5le%', '%C3%A5%able%', '%7B%ab%7C%de%7D', '%7B%ab%%7C%de%%7D', '%7 B%ab%%7C%de%%7 D', '%61+%4d%4D',
+    '\uFEFFtest', '\uFEFF', '%EF%BB%BFtest', '%EF%BB%BF', '†', '%C2%B5', '%C2%B5%', '%%C2%B5%', '%ab', '%ab%ab%ab',
+    '%', '%E0%A4%A', '/test/hel%"Flo', '/test/hel%2Flo'
+  ]
 
-  Object.keys(testCases).forEach(testCase => {
-    t.strictEqual(fastDecode(testCase), testCases[testCase])
+  uri.forEach(testCase => {
     try {
-      t.strictEqual(decodeURIComponent(testCase), testCases[testCase])
+      t.strictEqual(decodeURIComponent(testCase), fastDecode(testCase))
     } catch (e) {
-      // we support also '%' if is alone, eg '%7B%ab%7C%de%7D'
-      // here the url is composed as following:
-      // %7B %ab %7C %de %7D
-      // so '%ab' and '%de' will throw an error
-      // with the native decodeURIComponent
+      t.strictEqual(fastDecode(testCase), null)
     }
   })
 
