2accbd5549093b03b7c80b88f982bbca68feccfc
delvedor <tommydelved@gmail.com>
2018-03-19 10:44:26 +0100

    Updated test

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const { test } = require('tap')
 const randomstring = require('randomstring')
 const fastDecode = require('./index')
 
-const charset = 'abcdefghilmnopqrstuvzxywjk%ABCDEFHGILMNOPQRSTUVZXYWJK%0123456789.-_~%'
+const charset = 'abcdef_ghilmn%opqrstu-vzxywjk%ABCDEF_HGILMN%OPQRSTU-VZXYWJK%0123456789.-_~%'
 
 test('Basic', t => {
   // base test
@@ -12,11 +12,11 @@ test('Basic', t => {
     'test', 'a+b+c+d', '=a', '%25', '%%25%%', 'st%C3%A5le', 'st%C3%A5le%', '%st%C3%A5le%', '%%7Bst%C3%A5le%7D%',
     '%ab%C3%A5le%', '%C3%A5%able%', '%7B%ab%7C%de%7D', '%7B%ab%%7C%de%%7D', '%7 B%ab%%7C%de%%7 D', '%61+%4d%4D',
     '\uFEFFtest', '\uFEFF', '%EF%BB%BFtest', '%EF%BB%BF', '†', '%C2%B5', '%C2%B5%', '%%C2%B5%', '%ab', '%ab%ab%ab',
-    '%', '%2', '%E0%A4%A', '/test/hel%"Flo', '/test/hel%2Flo', 'qu4hxLjOOl79nEtzP7ypa%EFzoasD'
+    '%', '%2', '%E0%A4%A', '/test/hel%"Flo', '/test/hel%2Flo'
   ]
 
   // random generated uri
-  for (var i = 0; i < 10000; i++) {
+  for (var i = 0; i < 20000; i++) {
     uri.push(randomstring.generate({ charset }))
   }
 
